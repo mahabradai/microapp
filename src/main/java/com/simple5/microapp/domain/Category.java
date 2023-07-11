9a83c114e3f543ad1f46fdd6e84cc52371cf1fdf
mahabradai <br.maha.coder@gmail.com>
2023-07-11 11:50:13 +0200

    F.C

---
+++ src/main/java/com/simple5/microapp/domain/Category.java
@@ -0,0 +1,99 @@
+package com.simple5.microapp.domain;
+
+import java.io.Serializable;
+import javax.validation.constraints.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+/**
+ * A Category.
+ */
+@Table("category")
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column("id")
+    private Long id;
+
+    @NotNull(message = "must not be null")
+    @Column("name")
+    private String name;
+
+    @Column("description")
+    private String description;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Category id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Category name(String name) {
+        this.setName(name);
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Category description(String description) {
+        this.setDescription(description);
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Category)) {
+            return false;
+        }
+        return id != null && id.equals(((Category) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Category{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", description='" + getDescription() + "'" +
+            "}";
+    }
+}
---
+++ src/test/java/com/simple5/microapp/web/rest/CategoryResourceIT.java
@@ -0,0 +1,448 @@
+package com.simple5.microapp.web.rest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
+
+import com.simple5.microapp.IntegrationTest;
+import com.simple5.microapp.domain.Category;
+import com.simple5.microapp.repository.CategoryRepository;
+import com.simple5.microapp.repository.EntityManager;
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * Integration tests for the {@link CategoryResource} REST controller.
+ */
+@IntegrationTest
+@AutoConfigureWebTestClient(timeout = IntegrationTest.DEFAULT_ENTITY_TIMEOUT)
+@WithMockUser
+class CategoryResourceIT {
+
+    private static final String DEFAULT_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_NAME = "BBBBBBBBBB";
+
+    private static final String DEFAULT_DESCRIPTION = "AAAAAAAAAA";
+    private static final String UPDATED_DESCRIPTION = "BBBBBBBBBB";
+
+    private static final String ENTITY_API_URL = "/api/categories";
+    private static final String ENTITY_API_URL_ID = ENTITY_API_URL + "/{id}";
+
+    private static Random random = new Random();
+    private static AtomicLong count = new AtomicLong(random.nextInt() + (2 * Integer.MAX_VALUE));
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    private Category category;
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Category createEntity(EntityManager em) {
+        Category category = new Category().name(DEFAULT_NAME).description(DEFAULT_DESCRIPTION);
+        return category;
+    }
+
+    /**
+     * Create an updated entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Category createUpdatedEntity(EntityManager em) {
+        Category category = new Category().name(UPDATED_NAME).description(UPDATED_DESCRIPTION);
+        return category;
+    }
+
+    public static void deleteEntities(EntityManager em) {
+        try {
+            em.deleteAll(Category.class).block();
+        } catch (Exception e) {
+            // It can fail, if other entities are still referring this - it will be removed later.
+        }
+    }
+
+    @AfterEach
+    public void cleanup() {
+        deleteEntities(em);
+    }
+
+    @BeforeEach
+    public void initTest() {
+        deleteEntities(em);
+        category = createEntity(em);
+    }
+
+    @Test
+    void createCategory() throws Exception {
+        int databaseSizeBeforeCreate = categoryRepository.findAll().collectList().block().size();
+        // Create the Category
+        webTestClient
+            .post()
+            .uri(ENTITY_API_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(TestUtil.convertObjectToJsonBytes(category))
+            .exchange()
+            .expectStatus()
+            .isCreated();
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeCreate + 1);
+        Category testCategory = categoryList.get(categoryList.size() - 1);
+        assertThat(testCategory.getName()).isEqualTo(DEFAULT_NAME);
+        assertThat(testCategory.getDescription()).isEqualTo(DEFAULT_DESCRIPTION);
+    }
+
+    @Test
+    void createCategoryWithExistingId() throws Exception {
+        // Create the Category with an existing ID
+        category.setId(1L);
+
+        int databaseSizeBeforeCreate = categoryRepository.findAll().collectList().block().size();
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        webTestClient
+            .post()
+            .uri(ENTITY_API_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(TestUtil.convertObjectToJsonBytes(category))
+            .exchange()
+            .expectStatus()
+            .isBadRequest();
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    void checkNameIsRequired() throws Exception {
+        int databaseSizeBeforeTest = categoryRepository.findAll().collectList().block().size();
+        // set the field null
+        category.setName(null);
+
+        // Create the Category, which fails.
+
+        webTestClient
+            .post()
+            .uri(ENTITY_API_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(TestUtil.convertObjectToJsonBytes(category))
+            .exchange()
+            .expectStatus()
+            .isBadRequest();
+
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    void getAllCategories() {
+        // Initialize the database
+        categoryRepository.save(category).block();
+
+        // Get all the categoryList
+        webTestClient
+            .get()
+            .uri(ENTITY_API_URL + "?sort=id,desc")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectHeader()
+            .contentType(MediaType.APPLICATION_JSON)
+            .expectBody()
+            .jsonPath("$.[*].id")
+            .value(hasItem(category.getId().intValue()))
+            .jsonPath("$.[*].name")
+            .value(hasItem(DEFAULT_NAME))
+            .jsonPath("$.[*].description")
+            .value(hasItem(DEFAULT_DESCRIPTION));
+    }
+
+    @Test
+    void getCategory() {
+        // Initialize the database
+        categoryRepository.save(category).block();
+
+        // Get the category
+        webTestClient
+            .get()
+            .uri(ENTITY_API_URL_ID, category.getId())
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectHeader()
+            .contentType(MediaType.APPLICATION_JSON)
+            .expectBody()
+            .jsonPath("$.id")
+            .value(is(category.getId().intValue()))
+            .jsonPath("$.name")
+            .value(is(DEFAULT_NAME))
+            .jsonPath("$.description")
+            .value(is(DEFAULT_DESCRIPTION));
+    }
+
+    @Test
+    void getNonExistingCategory() {
+        // Get the category
+        webTestClient
+            .get()
+            .uri(ENTITY_API_URL_ID, Long.MAX_VALUE)
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus()
+            .isNotFound();
+    }
+
+    @Test
+    void putExistingCategory() throws Exception {
+        // Initialize the database
+        categoryRepository.save(category).block();
+
+        int databaseSizeBeforeUpdate = categoryRepository.findAll().collectList().block().size();
+
+        // Update the category
+        Category updatedCategory = categoryRepository.findById(category.getId()).block();
+        updatedCategory.name(UPDATED_NAME).description(UPDATED_DESCRIPTION);
+
+        webTestClient
+            .put()
+            .uri(ENTITY_API_URL_ID, updatedCategory.getId())
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(TestUtil.convertObjectToJsonBytes(updatedCategory))
+            .exchange()
+            .expectStatus()
+            .isOk();
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeUpdate);
+        Category testCategory = categoryList.get(categoryList.size() - 1);
+        assertThat(testCategory.getName()).isEqualTo(UPDATED_NAME);
+        assertThat(testCategory.getDescription()).isEqualTo(UPDATED_DESCRIPTION);
+    }
+
+    @Test
+    void putNonExistingCategory() throws Exception {
+        int databaseSizeBeforeUpdate = categoryRepository.findAll().collectList().block().size();
+        category.setId(count.incrementAndGet());
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        webTestClient
+            .put()
+            .uri(ENTITY_API_URL_ID, category.getId())
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(TestUtil.convertObjectToJsonBytes(category))
+            .exchange()
+            .expectStatus()
+            .isBadRequest();
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    void putWithIdMismatchCategory() throws Exception {
+        int databaseSizeBeforeUpdate = categoryRepository.findAll().collectList().block().size();
+        category.setId(count.incrementAndGet());
+
+        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
+        webTestClient
+            .put()
+            .uri(ENTITY_API_URL_ID, count.incrementAndGet())
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(TestUtil.convertObjectToJsonBytes(category))
+            .exchange()
+            .expectStatus()
+            .isBadRequest();
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    void putWithMissingIdPathParamCategory() throws Exception {
+        int databaseSizeBeforeUpdate = categoryRepository.findAll().collectList().block().size();
+        category.setId(count.incrementAndGet());
+
+        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
+        webTestClient
+            .put()
+            .uri(ENTITY_API_URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(TestUtil.convertObjectToJsonBytes(category))
+            .exchange()
+            .expectStatus()
+            .isEqualTo(405);
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    void partialUpdateCategoryWithPatch() throws Exception {
+        // Initialize the database
+        categoryRepository.save(category).block();
+
+        int databaseSizeBeforeUpdate = categoryRepository.findAll().collectList().block().size();
+
+        // Update the category using partial update
+        Category partialUpdatedCategory = new Category();
+        partialUpdatedCategory.setId(category.getId());
+
+        partialUpdatedCategory.name(UPDATED_NAME);
+
+        webTestClient
+            .patch()
+            .uri(ENTITY_API_URL_ID, partialUpdatedCategory.getId())
+            .contentType(MediaType.valueOf("application/merge-patch+json"))
+            .bodyValue(TestUtil.convertObjectToJsonBytes(partialUpdatedCategory))
+            .exchange()
+            .expectStatus()
+            .isOk();
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeUpdate);
+        Category testCategory = categoryList.get(categoryList.size() - 1);
+        assertThat(testCategory.getName()).isEqualTo(UPDATED_NAME);
+        assertThat(testCategory.getDescription()).isEqualTo(DEFAULT_DESCRIPTION);
+    }
+
+    @Test
+    void fullUpdateCategoryWithPatch() throws Exception {
+        // Initialize the database
+        categoryRepository.save(category).block();
+
+        int databaseSizeBeforeUpdate = categoryRepository.findAll().collectList().block().size();
+
+        // Update the category using partial update
+        Category partialUpdatedCategory = new Category();
+        partialUpdatedCategory.setId(category.getId());
+
+        partialUpdatedCategory.name(UPDATED_NAME).description(UPDATED_DESCRIPTION);
+
+        webTestClient
+            .patch()
+            .uri(ENTITY_API_URL_ID, partialUpdatedCategory.getId())
+            .contentType(MediaType.valueOf("application/merge-patch+json"))
+            .bodyValue(TestUtil.convertObjectToJsonBytes(partialUpdatedCategory))
+            .exchange()
+            .expectStatus()
+            .isOk();
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeUpdate);
+        Category testCategory = categoryList.get(categoryList.size() - 1);
+        assertThat(testCategory.getName()).isEqualTo(UPDATED_NAME);
+        assertThat(testCategory.getDescription()).isEqualTo(UPDATED_DESCRIPTION);
+    }
+
+    @Test
+    void patchNonExistingCategory() throws Exception {
+        int databaseSizeBeforeUpdate = categoryRepository.findAll().collectList().block().size();
+        category.setId(count.incrementAndGet());
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        webTestClient
+            .patch()
+            .uri(ENTITY_API_URL_ID, category.getId())
+            .contentType(MediaType.valueOf("application/merge-patch+json"))
+            .bodyValue(TestUtil.convertObjectToJsonBytes(category))
+            .exchange()
+            .expectStatus()
+            .isBadRequest();
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    void patchWithIdMismatchCategory() throws Exception {
+        int databaseSizeBeforeUpdate = categoryRepository.findAll().collectList().block().size();
+        category.setId(count.incrementAndGet());
+
+        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
+        webTestClient
+            .patch()
+            .uri(ENTITY_API_URL_ID, count.incrementAndGet())
+            .contentType(MediaType.valueOf("application/merge-patch+json"))
+            .bodyValue(TestUtil.convertObjectToJsonBytes(category))
+            .exchange()
+            .expectStatus()
+            .isBadRequest();
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    void patchWithMissingIdPathParamCategory() throws Exception {
+        int databaseSizeBeforeUpdate = categoryRepository.findAll().collectList().block().size();
+        category.setId(count.incrementAndGet());
+
+        // If url ID doesn't match entity ID, it will throw BadRequestAlertException
+        webTestClient
+            .patch()
+            .uri(ENTITY_API_URL)
+            .contentType(MediaType.valueOf("application/merge-patch+json"))
+            .bodyValue(TestUtil.convertObjectToJsonBytes(category))
+            .exchange()
+            .expectStatus()
+            .isEqualTo(405);
+
+        // Validate the Category in the database
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    void deleteCategory() {
+        // Initialize the database
+        categoryRepository.save(category).block();
+
+        int databaseSizeBeforeDelete = categoryRepository.findAll().collectList().block().size();
+
+        // Delete the category
+        webTestClient
+            .delete()
+            .uri(ENTITY_API_URL_ID, category.getId())
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus()
+            .isNoContent();
+
+        // Validate the database contains one less item
+        List<Category> categoryList = categoryRepository.findAll().collectList().block();
+        assertThat(categoryList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+}
